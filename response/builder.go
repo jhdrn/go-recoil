99dce557feffce984cc5a5a89b72f88480ed707a
Jonathan Hedrén <jonathan.hedren@gmail.com>
2024-03-20 13:37:54 +0100

    Change back Builder methods to work on copies rather than pointers.

--- response/builder.go
+++ response/builder.go
@@ -51,8 +51,8 @@ func WithConfig(c Config) Option {
 // NewBuilder returns a new response builder. By default it uses DefaultConfig
 // but can be configured to use another Config using the WithConfig function.
 // Will panic if the configuration is missing a formatter.
-func NewBuilder(options ...Option) *Builder {
-	builder := &Builder{
+func NewBuilder(options ...Option) Builder {
+	builder := Builder{
 		config: DefaultConfig,
 		responseData: ResponseData{
 			Header: make(http.Header),
@@ -61,7 +61,7 @@ func NewBuilder(options ...Option) *Builder {
 
 	// Apply all the functional options to configure the ALBApp.
 	for _, opt := range options {
-		opt(builder)
+		opt(&builder)
 	}
 
 	if builder.config.Formatter == nil {
@@ -72,38 +72,38 @@ func NewBuilder(options ...Option) *Builder {
 }
 
 // Body returns the content to be written to the response.
-func (r *Builder) Body() io.Reader {
+func (r Builder) Body() io.Reader {
 	return r.config.Formatter.FormatBody(r.responseData)
 }
 
 // Header returns the header map.
-func (r *Builder) Header() http.Header {
+func (r Builder) Header() http.Header {
 	return r.config.Formatter.FormatHeader(r.responseData)
 }
 
 // Status returns the HTTP status code. If no status code has been set,
 // 200 OK will be returned.
-func (r *Builder) Status() int {
+func (r Builder) Status() int {
 	return r.config.Formatter.FormatStatus(r.responseData)
 }
 
 // WithContent returns a copy of the response with the given content.
 // If the content argument implements the ResponseError interface,
 // the status will be set to the status of the ResponseError.
-func (r *Builder) WithContent(content any) *Builder {
+func (r Builder) WithContent(content any) Builder {
 	r.responseData.Body = content
 	return r
 }
 
 // WithStream returns a copy of the response with the given stream.
-func (r *Builder) WithStream(stream io.Reader) *Builder {
+func (r Builder) WithStream(stream io.Reader) Builder {
 	r.responseData.Body = stream
 	return r
 }
 
 // WithHeader returns a copy of the response with the given header. It will
 // replace the existing header.
-func (r *Builder) WithHeader(header http.Header) *Builder {
+func (r Builder) WithHeader(header http.Header) Builder {
 	r.responseData.Header = header
 	return r
 }
@@ -111,20 +111,20 @@ func (r *Builder) WithHeader(header http.Header) *Builder {
 // WithHeaderEntry returns a copy of the response with the given header entry.
 // If a header entry with the same key already exists, the existing values will
 // be replaced.
-func (r *Builder) WithHeaderEntry(key string, value ...string) *Builder {
+func (r Builder) WithHeaderEntry(key string, value ...string) Builder {
 	r.responseData.Header[key] = value
 	return r
 }
 
 // WithStatus returns a copy of the response with the given status.
-func (r *Builder) WithStatus(status int) *Builder {
+func (r Builder) WithStatus(status int) Builder {
 	r.responseData.Status = status
 	return r
 }
 
 // WithCookie returns a copy of the response with the given cookie set
 // to the response header.
-func (r *Builder) WithCookie(cookie *http.Cookie) *Builder {
+func (r Builder) WithCookie(cookie *http.Cookie) Builder {
 	if v := cookie.String(); v != "" {
 		r.WithHeaderEntry("Set-Cookie", v)
 	}
@@ -132,80 +132,80 @@ func (r *Builder) WithCookie(cookie *http.Cookie) *Builder {
 }
 
 // BadGateway returns a new response with the status code 502 Bad Gateway.
-func (r *Builder) BadGateway() *Builder {
+func (r Builder) BadGateway() Builder {
 	return r.WithStatus(http.StatusBadGateway)
 }
 
 // BadRequest returns a new response with the status code 400 Bad Request.
-func (r *Builder) BadRequest() *Builder {
+func (r Builder) BadRequest() Builder {
 	return r.WithStatus(http.StatusBadRequest)
 }
 
 // Conflict returns a new response with the status code 409 Conflict.
-func (r *Builder) Conflict() *Builder {
+func (r Builder) Conflict() Builder {
 	return r.WithStatus(http.StatusConflict)
 }
 
 // Created returns a new response with the status code 201 Created.
-func (r *Builder) Created() *Builder {
+func (r Builder) Created() Builder {
 	return r.WithStatus(http.StatusCreated)
 }
 
 // Forbidden returns a new response with the status code 403 Forbidden.
-func (r *Builder) Forbidden() *Builder {
+func (r Builder) Forbidden() Builder {
 	return r.WithStatus(http.StatusForbidden)
 }
 
 // Found returns a new response with the status code 302 Found
 // Permanently and the given location set as the Location header.
-func (r *Builder) Found(location string) *Builder {
+func (r Builder) Found(location string) Builder {
 	return r.WithStatus(http.StatusFound).WithHeaderEntry("Location", location)
 }
 
 // GatewayTimeout returns a new response with the status code 504 Gateway Timeout.
-func (r *Builder) GatewayTimeout() *Builder {
+func (r Builder) GatewayTimeout() Builder {
 	return r.WithStatus(http.StatusGatewayTimeout)
 }
 
 // Gone returns a new response with the status code 410 Gone.
-func (r *Builder) InternalServerError() *Builder {
+func (r Builder) InternalServerError() Builder {
 	return r.WithStatus(http.StatusInternalServerError)
 }
 
 // OK returns a new response with the status code 200 OK.
-func (r *Builder) OK() *Builder {
+func (r Builder) OK() Builder {
 	return r.WithStatus(http.StatusOK)
 }
 
 // MovedPermanently returns a new response with the status code 301 Moved
 // Permanently and the given location set as the Location header.
-func (r *Builder) MovedPermanently(location string) *Builder {
+func (r Builder) MovedPermanently(location string) Builder {
 	return r.WithStatus(http.StatusMovedPermanently).WithHeaderEntry("Location", location)
 }
 
 // NotFound returns a new response with the status code 404 Not Found.
-func (r *Builder) NotFound() *Builder {
+func (r Builder) NotFound() Builder {
 	return r.WithStatus(http.StatusNotFound)
 }
 
 // NotImplemented returns a new response with the status code 501 Not Implemented.
-func (r *Builder) NotImplemented() *Builder {
+func (r Builder) NotImplemented() Builder {
 	return r.WithStatus(http.StatusNotImplemented)
 }
 
 // PermanentRedirect returns a new response with the status code 308 Permanent Redirect
 // and the given location set as the Location header.
-func (r *Builder) PermanentRedirect(location string) *Builder {
+func (r Builder) PermanentRedirect(location string) Builder {
 	return r.WithStatus(http.StatusPermanentRedirect).WithHeaderEntry("Location", location)
 }
 
 // TemporaryRedirect returns a new response with the status code 308 Temporary Redirect
 // and the given location set as the Location header.
-func (r *Builder) TemporaryRedirect(location string) *Builder {
+func (r Builder) TemporaryRedirect(location string) Builder {
 	return r.WithStatus(http.StatusTemporaryRedirect).WithHeaderEntry("Location", location)
 }
 
 // Unauthorized returns a new response with the status code 401 Unauthorized.
-func (r *Builder) Unauthorized() *Builder {
+func (r Builder) Unauthorized() Builder {
 	return r.WithStatus(http.StatusUnauthorized)
 }
--- response/helpers.go
+++ response/helpers.go
@@ -1,86 +1,86 @@
 package response
 
-// Content creates a new *Builder with the supplied content using DefaultConfig.
-func Content(content any) *Builder {
+// Content creates a new Builder with the supplied content using DefaultConfig.
+func Content(content any) Builder {
 	return NewBuilder().WithContent(content)
 }
 
-// Status creates a new *Builder with the supplied status using DefaultConfig.
-func Status(status int) *Builder {
+// Status creates a new Builder with the supplied status using DefaultConfig.
+func Status(status int) Builder {
 	return NewBuilder().WithStatus(status)
 }
 
-// BadGateway returns a 502 Bad Gateway *Builder using DefaultConfig.
-func BadGateway() *Builder {
+// BadGateway returns a 502 Bad Gateway Builder using DefaultConfig.
+func BadGateway() Builder {
 	return NewBuilder().BadGateway()
 }
 
-// BadRequest returns a 400 Bad Request *Builder using DefaultConfig.
-func BadRequest() *Builder {
+// BadRequest returns a 400 Bad Request Builder using DefaultConfig.
+func BadRequest() Builder {
 	return NewBuilder().BadRequest()
 }
 
-// Conflict returns a 409 Conflict *Builder using DefaultConfig.
-func Conflict() *Builder {
+// Conflict returns a 409 Conflict Builder using DefaultConfig.
+func Conflict() Builder {
 	return NewBuilder().Conflict()
 }
 
-// Created returns a 201 Created *Builder using DefaultConfig.
-func Created() *Builder {
+// Created returns a 201 Created Builder using DefaultConfig.
+func Created() Builder {
 	return NewBuilder().Created()
 }
 
-// Forbidden returns a 403 Forbidden *Builder using DefaultConfig.
-func Forbidden() *Builder {
+// Forbidden returns a 403 Forbidden Builder using DefaultConfig.
+func Forbidden() Builder {
 	return NewBuilder().Forbidden()
 }
 
-// Found returns a 302 Found *Builder using DefaultConfig.
-func Found(location string) *Builder {
+// Found returns a 302 Found Builder using DefaultConfig.
+func Found(location string) Builder {
 	return NewBuilder().Found(location)
 }
 
-// GatewayTimeout returns a 504 Gateway Timeout *Builder using DefaultConfig.
-func GatewayTimeout() *Builder {
+// GatewayTimeout returns a 504 Gateway Timeout Builder using DefaultConfig.
+func GatewayTimeout() Builder {
 	return NewBuilder().GatewayTimeout()
 }
 
-// InternalServerError returns a 500 Internal Server Error *Builder using DefaultConfig.
-func InternalServerError() *Builder {
+// InternalServerError returns a 500 Internal Server Error Builder using DefaultConfig.
+func InternalServerError() Builder {
 	return NewBuilder().InternalServerError()
 }
 
-// OK returns a 200 OK *Builder using DefaultConfig.
-func OK() *Builder {
+// OK returns a 200 OK Builder using DefaultConfig.
+func OK() Builder {
 	return NewBuilder().OK()
 }
 
-// MovedPermanently returns a 301 Moved Permanently *Builder using DefaultConfig.
-func MovedPermanently(location string) *Builder {
+// MovedPermanently returns a 301 Moved Permanently Builder using DefaultConfig.
+func MovedPermanently(location string) Builder {
 	return NewBuilder().MovedPermanently(location)
 }
 
-// NotFound returns a 404 Not Found *Builder using DefaultConfig.
-func NotFound() *Builder {
+// NotFound returns a 404 Not Found Builder using DefaultConfig.
+func NotFound() Builder {
 	return NewBuilder().NotFound()
 }
 
-// NotImplemented returns a 501 Not Implemented *Builder using DefaultConfig.
-func NotImplemented() *Builder {
+// NotImplemented returns a 501 Not Implemented Builder using DefaultConfig.
+func NotImplemented() Builder {
 	return NewBuilder().NotImplemented()
 }
 
-// PermanentRedirect returns a 308 Permanent Redirect *Builder using DefaultConfig.
-func PermanentRedirect(location string) *Builder {
+// PermanentRedirect returns a 308 Permanent Redirect Builder using DefaultConfig.
+func PermanentRedirect(location string) Builder {
 	return NewBuilder().PermanentRedirect(location)
 }
 
-// TemporaryRedirect returns a 307 Temporary Redirect *Builder using DefaultConfig.
-func TemporaryRedirect(location string) *Builder {
+// TemporaryRedirect returns a 307 Temporary Redirect Builder using DefaultConfig.
+func TemporaryRedirect(location string) Builder {
 	return NewBuilder().TemporaryRedirect(location)
 }
 
-// Unauthorized returns a 401 Unauthorized *Builder using DefaultConfig.
-func Unauthorized() *Builder {
+// Unauthorized returns a 401 Unauthorized Builder using DefaultConfig.
+func Unauthorized() Builder {
 	return NewBuilder().Unauthorized()
 }
